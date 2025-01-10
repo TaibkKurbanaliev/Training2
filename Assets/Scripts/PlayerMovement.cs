313b03190489a4ecbe8c1dea1dde11e6358bc936
TaibkKurbanaliev <65422168+TaibkKurbanaliev@users.noreply.github.com>
2025-01-10 23:29:59 +0400

    Change move and add jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = 1.5f;
+    [SerializeField] private float _jumpForce = 10.0f;
+    [SerializeField] private int _layer;
 
     private Rigidbody _rb;
+    private bool _isGrounded = true;
 
     private void Start()
     {
@@ -19,8 +23,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        Vector2 direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        Debug.Log(direction);
-        _rb.AddForce(direction * _speed);
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        _rb.AddForce(direction.normalized * _speed, ForceMode.Force);
+
+        if (Input.GetKey(KeyCode.Space) && _isGrounded)
+        {
+            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnterw(Collision collision)
+    {
+        
+        if (collision.collider.gameObject.layer == _layer)
+        {
+            Debug.Log("Kek");
+            _isGrounded = true;
+        }
     }
 }
