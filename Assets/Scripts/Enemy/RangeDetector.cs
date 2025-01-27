2bd50ea5206b25132a961ecd016d59ce40bba72a
TaibkKurbanaliev <65422168+TaibkKurbanaliev@users.noreply.github.com>
2025-01-27 22:11:34 +0400

    Add player detection script

---
+++ Assets/Scripts/Enemy/RangeDetector.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RangeDetector : MonoBehaviour
+{
+    [SerializeField] private float _radius = 3.0f;
+    [SerializeField] private float _maxDistance = 10.0f;
+    [SerializeField] private LayerMask _target;
+
+    private RaycastHit _hit;
+    
+    private void Update()
+    {
+        if (Physics.SphereCast(transform.position, _radius, transform.forward, out _hit, _maxDistance, _target))
+        {
+            Debug.Log(_hit.collider.gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position + transform.forward * _maxDistance, _radius);
+    }
+}
