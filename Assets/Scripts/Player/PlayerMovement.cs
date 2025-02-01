9f1737f5118f06c8f03e579f83f2e7b4d80162c8
TaibkKurbanaliev <65422168+TaibkKurbanaliev@users.noreply.github.com>
2025-02-01 23:45:36 +0400

    Change movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = 1.5f;
+    [SerializeField] private float _acceleration = 4.0f;
+    [SerializeField] private float _deceleration = 2.0f;
     [SerializeField] private float _maxSpeed = 10.0f;
     [SerializeField] private float _minSpeed = 1.0f;
     [SerializeField] private float _jumpForce = 10.0f;
@@ -18,9 +20,11 @@ public class PlayerMovement : MonoBehaviour
     private PlayerAnimator _playerAnimator;
 
     private bool _isGrounded = false;
-    private float _smoothTime = 0.12f;
+    private float _smoothTime = 0.012f;
     private float _rotationVelocity;
     private float _verticalSpeed;
+    private InputSystem_Actions _inputActions;
+    private Vector2 _currentVelocity;
 
     public float Speed 
     { 
@@ -35,9 +39,11 @@ public class PlayerMovement : MonoBehaviour
     {
         _characterController = GetComponent<CharacterController>();
         _playerAnimator = GetComponent<PlayerAnimator>();
+        _inputActions = new InputSystem_Actions();
+        _inputActions.Enable();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         Gravity();
         Move();
@@ -45,18 +51,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void Gravity()
     {
-        _verticalSpeed = Mathf.Clamp(_verticalSpeed + _gravity * Time.deltaTime, _gravity, _jumpForce);
+        //_verticalSpeed = Mathf.Clamp(_verticalSpeed + _gravity * Time.deltaTime, _gravity, _jumpForce);
     }
 
     private void Move()
     {
-        var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        var input = _inputActions.Player.Move.ReadValue<Vector2>();
+        var targetDir = new Vector3(input.x, 0.0f, input.y);
 
-        var forwardSpeed = Mathf.Lerp(_characterController.velocity.y, _speed * input.y, _smoothTime);
-        var horizontalSpeed = Mathf.Lerp(_characterController.velocity.x, _speed * input.x, _smoothTime);
+        if (input.magnitude > 0.0f)
+        {
+            _currentVelocity = Vector2.Lerp(_currentVelocity, input * _speed, _acceleration * Time.deltaTime);
+        }
+        else
+        {
+            _currentVelocity = Vector2.Lerp(_currentVelocity, input * _speed, _deceleration * Time.deltaTime);
+        }
 
-        var direction = new Vector3(horizontalSpeed, 0.0f, forwardSpeed);
-        _characterController.Move(direction.normalized * Time.fixedDeltaTime + Vector3.up * _verticalSpeed);
+        _characterController.Move(new Vector3(_currentVelocity.x, 0.0f, _currentVelocity.y) * Time.deltaTime + Vector3.down * Time.deltaTime);
 
         Debug.Log(_characterController.velocity);
         _playerAnimator.SetMovementSpeed(new Vector2(_characterController.velocity.x, _characterController.velocity.z));
